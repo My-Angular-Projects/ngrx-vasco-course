5b84b161b442e4f9985c31cb53666c72091e672e
Valery Semenenko <gearmobile@gmail.com>
2024-05-30 18:01:49 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngrx-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-ngrx-vasco-course';
+}
